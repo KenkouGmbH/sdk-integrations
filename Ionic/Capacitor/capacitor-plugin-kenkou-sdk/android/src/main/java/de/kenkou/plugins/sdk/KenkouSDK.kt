8c5e11b2a49cf9aed9ef5b027dafd508d2cdfd78
carldatu <carldatu242@gmail.com>
2021-02-02 23:12:07 +0800

    add functions to android

--- Ionic/Capacitor/capacitor-plugin-kenkou-sdk/android/src/main/java/de/kenkou/plugins/sdk/KenkouSDK.kt
+++ Ionic/Capacitor/capacitor-plugin-kenkou-sdk/android/src/main/java/de/kenkou/plugins/sdk/KenkouSDK.kt
@@ -1,9 +1,21 @@
 package de.kenkou.plugins.sdk
 
 import com.getcapacitor.*
+import de.kenkou.measurement_sdk.KenkouSdk
+import de.kenkou.measurement_sdk.service.AuthTokenProvider
 
 @NativePlugin
 class KenkouSDK : Plugin() {
+
+    override fun load() {
+        super.load()
+        KenkouSdk.initialize(object : AuthTokenProvider() {
+            override fun onTokenRequest(listener: (String) -> Unit) {
+                listener.invoke("<your token>")
+            }
+        }, activity.applicationContext)
+    }
+
     @PluginMethod
     fun echo(call: PluginCall) {
         val value = call.getString("value")
@@ -11,4 +23,23 @@ class KenkouSDK : Plugin() {
         ret.put("value", value)
         call.success(ret)
     }
+
+    @PluginMethod
+    fun startMeasurement(call: PluginCall) {
+        KenkouSdk.startMeasurement(activity)
+        call.resolve()
+    }
+
+    @PluginMethod
+    fun clearUserData(call: PluginCall) {
+        KenkouSdk.clearUserData(activity)
+        call.resolve()
+    }
+
+    @PluginMethod
+    fun startMeasurementOnboarding(call: PluginCall) {
+        KenkouSdk.startMeasurementOnboarding(activity)
+        call.resolve()
+    }
+
 }
