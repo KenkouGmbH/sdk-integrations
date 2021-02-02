f102a3b38895e21ed84b35537c01cd36537cf758
carldatu <carldatu242@gmail.com>
2021-02-03 00:09:47 +0800

    add ios example

--- Ionic/Capacitor/capacitor-plugin-kenkou-sdk/ios/Plugin/Plugin.m
+++ Ionic/Capacitor/capacitor-plugin-kenkou-sdk/ios/Plugin/Plugin.m
@@ -5,4 +5,7 @@
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(KenkouSDK, "KenkouSDK",
            CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(startMeasurement, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(clearUserData, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(startMeasurementOnboarding, CAPPluginReturnPromise);
 )
