1f99eec032e19b967f58158b3a527f6b427e241d
aimane <aiman.chnaif@gmail.com>
2021-12-16 23:39:48 +1000

    generate kenkou-react-native-wrapper library project

---
+++ kenkou-react-native-wrapper/README.md
@@ -0,0 +1,27 @@
+# kenkou-react-native-wrapper
+
+Kenkou Measurement SDK for React Native
+
+## Installation
+
+```sh
+npm install kenkou-react-native-wrapper
+```
+
+## Usage
+
+```js
+import { multiply } from "kenkou-react-native-wrapper";
+
+// ...
+
+const result = await multiply(3, 7);
+```
+
+## Contributing
+
+See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
+
+## License
+
+MIT
---
+++ kenkou-react-native-wrapper/android/src/main/java/com/kenkoureactnativewrapper/KenkouReactNativeWrapperModule.kt
@@ -0,0 +1,24 @@
+package com.kenkoureactnativewrapper
+
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.Promise
+
+class KenkouReactNativeWrapperModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+
+    override fun getName(): String {
+        return "KenkouReactNativeWrapper"
+    }
+
+    // Example method
+    // See https://reactnative.dev/docs/native-modules-android
+    @ReactMethod
+    fun multiply(a: Int, b: Int, promise: Promise) {
+    
+      promise.resolve(a * b)
+    
+    }
+
+    
+}
